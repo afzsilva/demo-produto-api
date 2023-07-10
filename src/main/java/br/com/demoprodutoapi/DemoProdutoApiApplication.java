3b6eccabffba37273c65be88321c3424e9fab1ec
afzsilva <afzsilva@gmail.com>
2023-07-10 10:44:43 -0300

    Projeto inicio

---
+++ src/main/java/br/com/demoprodutoapi/DemoProdutoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.demoprodutoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProdutoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProdutoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/demoprodutoapi/DemoProdutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demoprodutoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProdutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
