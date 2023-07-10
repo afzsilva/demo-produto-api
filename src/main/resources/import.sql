24de6928771bafd8272474e5bd946be6b5b53da5
afzsilva <afzsilva@gmail.com>
2023-07-10 17:06:55 -0300

    Cria arquivo seed banco de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+-- H2
+--INSERT INTO produto(valor, id, descricao, nome) VALUES (5.00,1,'Produto natural','Arroz');
+--INSERT INTO produto(valor, id, descricao, nome) VALUES (5.00,2,'Produto natural','Feijão');
+--INSERT INTO produto(valor, id, descricao, nome) VALUES (5.00,3,'Produto natural','Farinha');
+
+-- MYSQL
+INSERT INTO produto(id,descricao,nome,valor)VALUES(1,'Produto Natural','Arroz',5.00);
+INSERT INTO produto(id,descricao,nome,valor)VALUES(2,'Produto Natural','Feijão',5.00);
+INSERT INTO produto(id,descricao,nome,valor)VALUES(3,'Produto Natural','Farinha',5.00);
