3fd1e89540018c067032eb4fd0bcc43d0ec54289
afzsilva <afzsilva@gmail.com>
2023-07-10 17:05:29 -0300

    Implementa metodos service

--- src/main/java/br/com/demoprodutoapi/services/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/demoprodutoapi/services/impl/ProdutoServiceImpl.java
@@ -2,23 +2,40 @@ package br.com.demoprodutoapi.services.impl;
 
 import br.com.demoprodutoapi.dto.ProdutoDTO;
 import br.com.demoprodutoapi.entities.Produto;
+import br.com.demoprodutoapi.mapper.impl.dto.ProdutoMapper;
+import br.com.demoprodutoapi.repositories.ProdutoRepository;
 import br.com.demoprodutoapi.services.ProdutoService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@AllArgsConstructor
+@Transactional
 public class ProdutoServiceImpl implements ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+    private final ProdutoMapper produtoMapper;
+
     @Override
     public List<ProdutoDTO> buscarTodos() {
-        return null;
+        return produtoMapper.entidadesParaDTOs(produtoRepository.findAll());
     }
 
     @Override
-    public ProdutoDTO salvar(Produto produto) {
-        return null;
+    public ProdutoDTO salvar(ProdutoDTO produtoDTO) {
+        Produto produto = produtoMapper.dtoParaEntidade(produtoDTO);
+        produto = produtoRepository.save(produto);
+        produtoDTO.setId(produto.getId());
+        return produtoDTO;
     }
 
     @Override
-    public void delete(ProdutoDTO produtoDTO, Long id) {
-
+    public void delete(Long id) {
+       Produto produto = produtoRepository.findById(id).orElseThrow(()-> new RuntimeException("Impossivel remover, Produto não existe"));
+       produtoRepository.deleteById(produto.getId());
     }
 }
